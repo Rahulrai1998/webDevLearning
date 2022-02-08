9f93648c8bb93183dfbaf76a604034030f7c0e0e
Rahul Kumar <rahulrai2822march@gmail.com>
2022-02-08 17:29:03 +0530

    updated

--- new/Express/app.js
+++ new/Express/app.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 80;
 
+app.set('view engine','pug');
+app.set('views',path.join(__dirname , 'views'));
+app.get("/demo", (req,res)=>{
+    res.render('demo' , {title:'first pug application' , message:'Hello there man'})
+});
+
+
+app.use('/static',express.static('static'));
+
 app.get("/" , (req,res)=>{
-    res.send("this is home of my first-express app")
+    res.send("this   of my first-express app")
 });
 app.get("/about" , (req,res)=>{
     res.send("this is About section of my first-express app")
@@ -12,7 +22,7 @@ app.post("/about" , (req,res)=>{
     res.status(404).send("this is POst request About section of my first-express app")
 });
 
-app.listen(port , ()=>{
-
-    console.log(`This app is running on port ${port}`);
-});
+app.listen(port,()=> {
+    console.log(`Server running at ${port}`);
+  });
+  
