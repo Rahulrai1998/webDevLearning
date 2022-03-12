3529fc0543a23118060107e473004852246b1296
Rahul Kumar <rahulrai2822march@gmail.com>
2022-03-12 14:32:52 +0530

    updated

---
+++ new/MongooseTut/index.js
@@ -0,0 +1,14 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/myDB');
+}
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema); 
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
---
+++ new/tut81.js
@@ -0,0 +1,2 @@
+db.items.insertMany([{name:'Samsung 30s',price:22000,rating:4.5,qty:233,sold:98},{name:"Moto 30s",price:29000,rating:3.5,qty:133,sold:598},
+{name:"Realme 80s",price:79000,rating:3.2,qty:203,sold:98}])
