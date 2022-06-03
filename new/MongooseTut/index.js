c8ecbc757834f7161a925ba441db8fb0b63abc1f
Rahulrai1998 <rahulrai2822march@gmail.com>
2022-06-03 22:46:41 +0530

    react tutorial started

--- new/MongooseTut/index.js
+++ new/MongooseTut/index.js
@@ -1,14 +1,21 @@
-// getting-started.js
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/myDB');
+    mongoose.connect('mongodb://localhost:27017/test');
 }
+
 const kittySchema = new mongoose.Schema({
     name: String
-});
-const Kitten = mongoose.model('Kitten', kittySchema); 
+}); const Kitten = mongoose.model('Kitten', kittySchema);
 const silence = new Kitten({ name: 'Silence' });
 console.log(silence.name); // 'Silence'
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+
+const fluffy = new Kitten({ name: 'fluffy' });
+
+fluffy.save();
+
+const kittens = Kitten.find();
+console.log(kittens);
